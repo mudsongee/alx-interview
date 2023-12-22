23d6aad5c46ebff71509b6ce09cb3c384e350338
mudsongee <mohdahmedjafar@gmail.com>
2023-12-22 02:30:08 +0100

    Log Parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,39 +1,60 @@
 #!/usr/bin/python3
-"""
-Log parsing
-"""
+
 
 import sys
+import signal
+# Function to handle interrupt (CTRL+C)
 
-if __name__ == '__main__':
 
-    filesize, count = 0, 0
-    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
-    stats = {k: 0 for k in codes}
+def signal_handler(signal, frame):
+    print_stats()
+    sys.exit(0)
 
-    def print_stats(stats: dict, file_size: int) -> None:
-        print("File size: {:d}".format(filesize))
-        for k, v in sorted(stats.items()):
-            if v:
-                print("{}: {}".format(k, v))
+# Function to print statistics
 
-    try:
-        for line in sys.stdin:
-            count += 1
-            data = line.split()
-            try:
-                status_code = data[-2]
-                if status_code in stats:
-                    stats[status_code] += 1
-            except BaseException:
-                pass
-            try:
-                filesize += int(data[-1])
-            except BaseException:
-                pass
-            if count % 10 == 0:
-                print_stats(stats, filesize)
-        print_stats(stats, filesize)
-    except KeyboardInterrupt:
-        print_stats(stats, filesize)
-        raise
+
+def print_stats():
+    print("File size: {}".format(total_size))
+    for code in sorted(status_codes.keys()):
+        print("{}: {}".format(code, status_codes[code]))
+
+
+# Initialize variables
+total_size = 0
+status_codes = {}
+
+# Set up signal handler for interrupt
+signal.signal(signal.SIGINT, signal_handler)
+
+try:
+    # Process input line by line
+    for i, line in enumerate(sys.stdin, 1):
+        try:
+            # Parse the input line
+            parts = line.split()
+            ip_address = parts[0]
+            status_code = int(parts[-2])
+            file_size = int(parts[-1])
+
+            # Update total file size
+            total_size += file_size
+
+            # Update status code count
+            if status_code in [200, 301, 400, 401, 403, 404, 405, 500]:
+                if status_code not in status_codes:
+                    status_codes[status_code] = 1
+                else:
+                    status_codes[status_code] += 1
+
+            # Print statistics every 10 lines
+            if i % 10 == 0:
+                print_stats()
+
+        except (ValueError, IndexError):
+            # Skip lines with incorrect format
+            continue
+
+except KeyboardInterrupt:
+    # Handle keyboard interrupt (CTRL+C)
+    print_stats()
+    sys.exit(0)
